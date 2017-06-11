d8ec952551c9260f60097e0ef482bcf87acbe343
alok1994 <verma.alok606@gmail.com>
2017-06-11 09:32:02 -0700

    Initial Commit

---
+++ test_basic_cases.py
@@ -0,0 +1,145 @@
+import pytest
+import unittest
+import json
+import wapi_module
+import re
+import time
+import util
+import os
+from netaddr import IPNetwork
+
+tenant_name = 'admin'
+network = 'net1'
+subnet_name = "Snet"
+subnet = "10.2.0.0/24"
+
+class TestOpenStackCases(unittest.TestCase):
+    @classmethod
+    def setup_class(cls):
+	pass
+
+    @pytest.mark.run(order=1)	
+    def test_create_Network_OpenStack_Side(self):
+        proc = util.utils()
+        proc.create_network(network)
+	flag = proc.get_network(network)
+	assert flag == network
+
+    @pytest.mark.run(order=2)
+    def test_create_subnet_openstack_side(self):
+	proc = util.utils()
+	proc.create_subnet(network, subnet_name, subnet)
+	flag = proc.get_subnet_name(subnet_name)
+	assert flag == subnet_name
+
+    @pytest.mark.run(order=3)
+    def test_validate_network_on_NIOS(self):
+	flag = False	
+	proc = wapi_module.wapi_request('GET',object_type = 'network',params="?network="+subnet)
+	if (re.search(r""+subnet,proc)):
+	    flag = True
+	assert flag, "Network creation failed "
+
+    @pytest.mark.run(order=4)
+    def test_validate_NIOS_EAs_Cloud_API_Owned_CMP_Type(self):
+	proc = wapi_module.wapi_request('GET',object_type = 'network',params="?network="+subnet)
+	resp = json.loads(proc)
+	ref_v = resp[0]['_ref']
+	EAs = json.loads(wapi_module.wapi_request('GET',object_type = ref_v + '?_return_fields=extattrs'))
+	assert EAs['extattrs']['Cloud API Owned']['value'] == 'True' and EAs['extattrs']['CMP Type']['value'] == 'OpenStack'
+
+    @pytest.mark.run(order=5)
+    def test_Validate_NIOS_EAs_Network_Name_Network_ID_Subnet_Name_Subnet_ID(self):
+        proc = wapi_module.wapi_request('GET',object_type = 'network',params="?network="+subnet)
+	session = util.utils()
+	Net_name = session.get_network(network)
+	Net_id = session.get_network_id(network)
+	Sub_name = session.get_subnet_name(subnet_name)
+	Snet_ID = session.get_subnet_id(subnet_name)
+	resp = json.loads(proc)
+        ref_v = resp[0]['_ref']
+        EAs = json.loads(wapi_module.wapi_request('GET',object_type = ref_v + '?_return_fields=extattrs'))
+        assert EAs['extattrs']['Network Name']['value'] == Net_name and \
+               EAs['extattrs']['Network ID']['value'] == Net_id and \
+               EAs['extattrs']['Subnet Name']['value'] == Sub_name and \
+               EAs['extattrs']['Subnet ID']['value'] == Snet_ID and \
+               EAs['extattrs']['Network Encap']['value'] == 'vxlan' 
+
+    @pytest.mark.run(order=6)
+    def test_validate_NIOS_EAs_Tenant_ID_Tenant_Name(self):
+	proc = wapi_module.wapi_request('GET',object_type = 'network',params="?network="+subnet)
+        session = util.utils()
+        tenant_id = session.get_tenant_id(network)
+	resp = json.loads(proc)
+        ref_v = resp[0]['_ref']
+        EAs = json.loads(wapi_module.wapi_request('GET',object_type = ref_v + '?_return_fields=extattrs'))
+        assert EAs['extattrs']['Tenant ID']['value'] == tenant_id and \
+	       EAs['extattrs']['Tenant Name']['value'] == 'admin'
+
+    @pytest.mark.run(order=7)
+    def test_validate_NIOS_Router(self):
+	proc = wapi_module.wapi_request('GET',object_type = 'network',params="?network="+subnet)
+	resp = json.loads(proc)
+        ref_v = resp[0]['_ref']
+	options = json.loads(wapi_module.wapi_request('GET',object_type = ref_v + '?_return_fields=options'))
+	route_list = options['options']
+	list_route = route_list[1]
+	route_nios = list_route['value']
+	ip = IPNetwork(subnet).iter_hosts()
+        route = str(ip.next())
+	assert route_nios == route
+
+    @pytest.mark.run(order=8)
+    def test_delete_net_subnet_openstack_side(self):
+        session = util.utils()
+	delete_net = session.delete_network(network)
+	assert delete_net == None
+
+    @pytest.mark.run(order=9)
+    def test_validate_delete_network_on_NIOS(self):
+        flag = True
+        proc = wapi_module.wapi_request('GET',object_type = 'network',params="?network="+subnet)
+        if (re.search(r""+subnet,proc)):
+            flag = False
+        assert flag, "Network didn't remove from NIOS "
+
+    @pytest.mark.run(order=10)
+    def test_enable_DHCP_DNS_support_Default_Domain_Name_Pattern_TENENT_NAME_EAs(self):
+	ref_v = json.loads(wapi_module.wapi_request('GET',object_type='member'))
+	ref = ref_v[0]['_ref']
+	data = {"extattrs":{"Admin Network Deletion": {"value": "True"},\
+                "Allow Service Restart": {"value": "True"},\
+                "Allow Static Zone Deletion":{"value": "True"},"DHCP Support": {"value": "True"},\
+                "DNS Record Binding Types": {"value":["record:a","record:aaaa","record:ptr"]},\
+                "DNS Record Removable Types": {"value": ["record:a","record:aaaa","record:ptr","record:txt"]},\
+                "DNS Record Unbinding Types": {"value": ["record:a","record:aaaa","record:ptr"]},\
+                "DNS Support": {"value": "True"},"DNS View": {"value": "default"},\
+                "Default Domain Name Pattern": {"value": "{tenant_name}.cloud.global.com"},\
+                "Default Host Name Pattern": {"value": "host-{ip_address}"},\
+                "Default Network View": {"value": "default"},\
+                "Default Network View Scope": {"value": "Single"},\
+                "External Domain Name Pattern": {"value": "{subnet_id}.external.global.com"},\
+                "External Host Name Pattern": {"value": "{instance_name}"},\
+                "Grid Sync Maximum Wait Time": {"value": 10},\
+                "Grid Sync Minimum Wait Time": {"value": 10},"Grid Sync Support": {"value": "True"},\
+                "IP Allocation Strategy": {"value": "Fixed Address"},\
+                "Relay Support": {"value": "False"},\
+                "Report Grid Sync Time": {"value": "True"},\
+                "Tenant Name Persistence": {"value": "False"},\
+                "Use Grid Master for DHCP": {"value": "True"},\
+                "Zone Creation Strategy": {"value": ["Forward","Reverse"]}}} 
+	proc = wapi_module.wapi_request('PUT',object_type=ref,fields=json.dumps(data))
+	flag = False
+        if (re.search(r"infoblox.localdomain",proc)):
+            flag = True
+        assert proc != "" and flag
+
+    @pytest.mark.run(order=11)
+    def test_create_network_DHCP_DNS_support_Domain_Name_TENANT_Name_openstack_side(self):
+	proc = util.utils()
+        proc.create_network(network)
+	proc.create_subnet(network, subnet_name, subnet)
+        flag = proc.get_subnet_name(subnet_name)
+	flag = proc.get_subnet_name(subnet_name)
+        assert flag == subnet_name
+
---
+++ util.py
@@ -0,0 +1,207 @@
+from keystoneauth1 import identity
+from keystoneauth1 import session
+from neutronclient.v2_0 import client
+import os,sys
+from novaclient.client import Client
+
+#os.system('export OS_USERNAME=admin')
+#os.system('export OS_PASSWORD=admin')
+#os.system('export OS_TENANT_NAME=admin')
+#os.system('export OS_AUTH_URL=http://10.39.12.231:35357/v3')
+class utils:
+	def __init__(self):
+            username='admin'
+            password='admin'
+            project_name='admin'
+            project_domain_id='default'
+            user_domain_id='default'
+            auth_url='http://10.39.12.231:5000/v3'
+	    VERSION = '2'
+            auth = identity.Password(auth_url=auth_url,
+                             username=username,
+                             password=password,
+                             project_name=project_name,
+                             project_domain_id=project_domain_id,
+                             user_domain_id=user_domain_id)
+            sess = session.Session(auth = auth)
+            self.neutron = client.Client(session=sess)
+	    self.nova_client = Client(VERSION,session=sess)
+
+	def create_network(self,name, external=False):
+	    ''''
+	    Add a Network OpenStack Side
+	      Pass the Network Name as Arg
+	    '''
+            network = {'network': {'name': name, 'admin_state_up': True, 'router:external' : external}}
+            netw = self.neutron.create_network(body=network)
+	
+	def get_network(self,name):
+	    networks = self.neutron.list_networks(name)
+	    net_name = networks['networks'][0]['name']
+	    return net_name
+
+	def get_network_id(self,name):
+	    networks = self.neutron.list_networks(name)
+	    network_id = networks['networks'][0]['id']
+	    return network_id
+
+	def get_tenant_id(self,name):
+	    tenant = self.neutron.list_networks(name)
+            tenant_id = tenant['networks'][0]['tenant_id']
+            return tenant_id
+
+	def delete_network(self,name):
+            networks = self.neutron.list_networks(name)
+            network_id = networks['networks'][0]['id']
+	    delete_net = self.neutron.delete_network(network_id)
+            return None
+	
+	def create_subnet(self, network_name, subnet_name, subnet):
+            """
+               Creates a Subnet
+               It takes Network Name, Subnet Name and Subnet as arguments.
+               For Example:-
+               project.create_subnet("Network1", "Subnet-1-1", "45.0.0.0/24")
+            """
+            net_id = self.get_network_id(network_name)
+	    tenant_id = self.get_tenant_id(network_name)
+            body_create_subnet = {'subnets': [{'name': subnet_name, 'cidr': subnet, 'ip_version': 4,\
+                                  'tenant_id': tenant_id, 'network_id': net_id}]}
+            subnet = self.neutron.create_subnet(body=body_create_subnet)
+
+	def get_subnet_name(self,subnet_name):
+            subnets = self.neutron.list_subnets(subnet_name)
+            sub_name = subnets['subnets'][0]['name']
+            return sub_name
+
+        def get_subnet_id(self,subnet_name):
+            subnets = self.neutron.list_subnets(subnet_name)
+            sub_id = subnets['subnets'][0]['id']
+            return sub_id
+
+	def delete_subnet(self,subnet_name):
+ 	    subnets = self.neutron.list_subnets(subnet_name)
+            sub_id = subnets['subnets'][0]['id']
+	    delete_sub = self.neutron.delete_subnets(sub_id)
+	    return None	    
+
+        def launch_instance(self, name, net_name):
+            """
+              Return Server Object if the instance is launched successfully
+        
+              It takes Instance Name and the Network Name it should be associated with as arguments.
+            """
+            image = self.nova_client.images.find(name="cirros-0.3.3-x86_64-disk")
+            flavor = self.nova_client.flavors.find(name="m1.tiny")
+            net_id  = self.get_network_id(net_name)
+            instance = self.nova_client.servers.create(name=name, image=image,\
+                                                       flavor=flavor, nics=nic_d)
+            time.sleep(60)
+            return instance
+
+        def get_servers_list(self):
+            """
+              Return List of Servers
+            """
+            return self.nova_client.servers.list()
+
+    	def get_server(self, name):
+            """
+              Return Server Object for a given instance name
+            """
+            servers_list = self.get_servers_list()
+            server_exists = False
+            for s in servers_list:
+                if s.name == name:
+                    server_exists = True
+                    break
+            if not server_exists:
+                return None
+            else:
+                return s
+
+	def terminate_instance(self, name):
+            """
+              Terminates an instance
+              It takes Instance Name as argument.
+            """
+            server = self.get_server(name)
+            if server:
+               self.nova_client.servers.delete(server)
+            time.sleep(60)
+
+    	def create_router(self, router_name, network_name):
+            net_id = self.get_network_id(network_name)
+            route = {'router': {'name': router_name, 'admin_state_up': True, 'external_gateway_info': {'network_id': net_id1}}}
+            router = self.neutron.create_router(body=route)
+
+	def get_routers_name(self,router_name):
+            routers = self.neutron.list_routers(router_name)
+	    router = routers['routers'][0]['name']
+            return router
+
+    	def get_router_id(self, route_name):
+	    routers = self.neutron.list_router()
+	    router_id = routers['routers'][0]['id']
+	    return router_id
+
+        def delete_router(self, router_name):
+            router_id = self.get_router_id(router_name)
+            self.neutron.delete_router(router=router_id)
+
+    	def create_port(self, interface_name, network_name):
+            net_id = self.get_network_id(network_name)
+            port = {'port': {'name': interface_name, 'admin_state_up': True, 'network_id': net_id}}
+            port_info = self.neutron.create_port(body=port)
+
+    	def get_ports(self,interface_name):
+            ports = self.neutron.list_ports(interface_name)
+            port_name = ports['ports'][0]['name']
+	    return port_name
+
+    	def get_port_id(self, interface_name):
+	    ports = self.neutron.list_ports(interface_name)
+            port_id = ports['ports'][0]['id']
+	    return port_id
+
+    	def add_router_interface(self, interface_name, router_name):
+            router_id = self.get_router_id(router_name)
+            port_id = self.get_port_id(interface_name)
+            body = {'port_id':port_id}
+            router = self.neutron.add_interface_router(router=router_id, body=body)
+
+    	def remove_router_interface(self, interface_name, router_name):
+            router_id = self.get_router_id(router_name)
+            port_id = self.get_port_id(interface_name)
+            body = {'port_id':port_id}
+            router = self.neutron_client.remove_interface_router(router=router_id, body=body)
+
+    	def add_floating_ip(self, instance_name):
+            floating_ip = self.nova_client.floating_ips.create()
+            instance = self.nova_client.servers.find(name=instance_name)
+            instance.add_floating_ip(floating_ip)
+
+    	def delete_floating_ip(self, instance_name):
+            fip_list = self.nova_client.floating_ips.list()
+            floating_ip = fip_list[0].ip
+            floating_ip_id = fip_list[0].id
+            instance = self.nova_client.servers.find(name=instance_name)
+            instance.remove_floating_ip(floating_ip)
+            self.nova_client.floating_ips.delete(floating_ip_id)
+
+	def get_instance_name(self, instance):
+            name = instance.name
+            return name
+
+    	def get_instance_ips(self, instance_name):
+            instance = self.nova_client.servers.find(name=instance_name) 
+            ips = self.nova_client.servers.ips(instance.id)
+            return ips
+
+    	def interface_attach(self, server, network):
+            net_id = self.get_network_id(network)
+            iface = self.nova_client.servers.interface_attach(port_id='',fixed_ip='',server=server,net_id=net_id,)
+            return iface
+
+    	def interface_detach(self, server, port_id):
+            self.nova_client.servers.interface_detach(server=server, port_id=port_id) 
