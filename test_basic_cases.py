0f874e0375492e616b2b36ac707b7fbdb837c663
alok1994 <alokverma067@gmail.com>
2017-06-23 05:49:32 -0400

    Add terminate_instance_used_SubnetName_as_DomainNamePattern_and_NetworkName_as_HostNamePattern case

--- test_basic_cases.py
+++ test_basic_cases.py
@@ -659,4 +659,15 @@ class TestOpenStackCases(unittest.TestCase):
         fqdn = "host-"+network_name+'-'+'-'.join(ip_address.split('.'))+'.'+zone_name
         assert fqdn == a_record_name
 
+    @pytest.mark.run(order=46)
+    def test_terminate_instance_used_SubnetName_as_DomainNamePattern_and_NetworkName_as_HostNamePattern(self):
+        proc = util.utils()
+        server = proc.terminate_instance()
+        assert server == None
+
+    @pytest.mark.run(order=47)
+    def test_delete_subnet_used_SubnetName_as_DomainNamePattern(self):
+        session = util.utils()
+	delete_net = session.delete_network(network)
+	assert delete_net == None
 
