af4c08f3ed7fde555d809755387049660387bf5d
alok1994 <alokverma067@gmail.com>
2017-06-30 09:47:05 -0400

    Added IPAllocationStrategy_as_HostReocrd cases

--- test_tool.py
+++ test_tool.py
@@ -9,8 +9,6 @@ import os
 from netaddr import IPNetwork
 import commands
 import os, subprocess as sp, json
-import os, pickle
-
 import subprocess
 
 tenant_name = 'admin'
@@ -41,44 +39,20 @@ class TestOpenStackCases(unittest.TestCase):
 
     @classmethod
     def setup_class(cls):
-        # source("/opt/devstack/openrc", "admin", "admin")
         source("/home/stack/keystone_admin")
 	pass 
-	#cmd = '/bin/bash -c "source /opt/devstack/openrc admin admin"'
-        #commands.getoutput(cmd)
-        #command = ['bash', '-c', 'source /opt/devstack/openrc admin admin']
-        #proc = subprocess.Popen(command, stdout = subprocess.PIPE)
-        #for line in proc.stdout:
-        #    (key, _, value) = line.partition("=")
-        #    os.environ[key] = value
-        #proc.communicate()
-        #pprint.pprint(dict(os.environ)
-
-    @pytest.mark.run(order=5)
-    def test_RUNSource(self):
-        
-        #bash.execute('a-tool.sh --args')
-	#cmd = 'export OS_USERNAME=admin'
-        #cmd = 'source /opt/devstack/openrc admin admin'
-        #os.system(cmd)
-	#source = 'source /opt/devstack/openrc admin admin'
-        #dump = '/usr/bin/python -c "import os, json;print json.dumps(dict(os.environ))"'
-        #pipe = sp.Popen(['/bin/bash', '-c', '%s && %s' %(source,dump)], stdout=sp.PIPE)
-        #env = json.loads(pipe.stdout.read())
-        #os.environ = env
-	pass
 
     @pytest.mark.run(order=6)
     def test_RUNSYNCTool_network_DomainNamePattern_as_TenantName_EAs_sync_tool(self):
-        import os
-        log = open('log.txt', 'w')
-        print >> log, os.environ["OS_USERNAME"]
-        # cmd = 'python /opt/stack/networking-infoblox/networking_infoblox/tools/sync_neutron_to_infoblox.py >> log.txt'
-	# proc = commands.getoutput(cmd)
+        #import os
+        #log = open('log.txt', 'w')
+        #print >> log, os.environ["OS_USERNAME"]
+        cmd = 'python /opt/stack/networking-infoblox/networking_infoblox/tools/sync_neutron_to_infoblox.py >> log.txt'
+	proc = commands.getoutput(cmd)
 	# time.sleep(20)
 	# import pdb;pdb.set_trace()
-	a=open('log.txt','rb')
-        lines = a.readlines()
-        if lines:
-           last_line = lines[-1]	
+	#a=open('log.txt','rb')
+        #lines = a.readlines()
+        #if lines:
+        #   last_line = lines[-1]	
 
